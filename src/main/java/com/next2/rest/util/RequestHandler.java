a843800b569ff10ae75cf88f1b73ee9d306d2f11
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-10-24 10:34:11 +0200

    Better logging in RequestHandler
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- src/main/java/com/next2/rest/util/RequestHandler.java
+++ src/main/java/com/next2/rest/util/RequestHandler.java
@@ -1,14 +1,20 @@
 package com.next2.rest.util;
 
+import com.next2.rest.api.Countries;
+import org.apache.log4j.Logger;
+
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.Response;
 
 public class RequestHandler {
 
     public static Response GET(Invocation.Builder invocation) {
+        final Logger LOG = Logger.getLogger(Response.class);
+
         Response response = invocation.get();
         if (response.getStatus() != 200) {
-            System.out.println("Failed to request the source. Implement retry logic here");
+            LOG.error("Failed to request the source. Implement retry logic here");
+            LOG.error("Error: " + response.toString());
             return null;
         }
         return response;
